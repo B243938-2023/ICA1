a80b8b4ef221fc84d09c095efb83899ab03d5e52
B243938-2023 <email_not_provided>
2023-10-16 00:33:34 +0100

    bowtie2 mapping, convert outputfiles to bam format and sort; generate counts data using bedtools

--- project.sh
+++ project.sh
@@ -22,3 +22,26 @@ gunzip -k ${PWD}/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.
 rm -f ${PWD}/Tcongo_genome/*.bt2
 bowtie2-build ${PWD}/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta ${PWD}/Tcongo_genome/TcongolenseIL3000_2019_Genome
 
+#bowtie2 mapping and convert outputfile to bam format and sort
+IFS="_";
+rm -fr ${PWD}/fastq/alignoutput
+mkdir ${PWD}/fastq/alignoutput
+alignoutput=${PWD}/fastq/alignoutput
+while read Tcopart1 Tcopart2
+do
+bowtie2 -p 10 -x ${PWD}/Tcongo_genome/TcongolenseIL3000_2019_Genome -1 ${PWD}/fastq/${Tcopart1}_1.fq.gz  -2 ${PWD}/fastq/${Tcopart1}_2.fq.gz -S ${alignoutput}/${Tcopart1}.sam
+samtools view -bh -S ${alignoutput}/${Tcopart1}.sam > ${alignoutput}/${Tcopart1}.bam
+samtools sort -O BAM -m 8g -@ 6 -o ${alignoutput}/${Tcopart1}_sort.bam ${alignoutput}/${Tcopart1}.bam
+done < ${PWD}/fastq/sequence_file.txt
+
+rm -rf ${PWD}/genecount
+mkdir ${PWD}/genecount
+
+# generate counts data using bedtools
+while read Tcopart1 Tcopart2
+do
+bedtools intersect -a TriTrypDB-46_TcongolenseIL3000_2019.bed -b ${PWD}/fastq/alignoutput/${Tcopart1}_sort.bam -c > ${PWD}/genecount/${Tcopart1}.gene.counts.txt
+done < ${PWD}/fastq/sequence_file.txt
+unset IFS
+
+
