9c6bb7685265bbb285c61af4f098a592152e87ca
B243938-2023 <email_not_provided>
2023-10-22 01:57:08 +0100

    calculate the mean expression levels for each group

--- ica1script.sh
+++ ica1script.sh
@@ -46,12 +46,12 @@ unset IFS
 
 # 4 generate counts data using bedtools
 IFS=$'_'
-rm -fr ${PWD}/genecount
-mkdir ${PWD}/genecount
-genecount=${PWD}/genecount
+rm -fr ${PWD}/genecounts
+mkdir ${PWD}/genecounts
+genecounts=${PWD}/genecounts
 while read Tcopart1 Tcopart2
 do
-bedtools coverage -a TriTrypDB-46_TcongolenseIL3000_2019.bed -b ${PWD}/fastq/alignoutput/${Tcopart1}_sort.bam > ${genecount}/${Tcopart1}.gene.counts.txt
+bedtools coverage -a TriTrypDB-46_TcongolenseIL3000_2019.bed -b ${PWD}/fastq/alignoutput/${Tcopart1}_sort.bam > ${genecounts}/${Tcopart1}.gene.counts.txt
 done < ${PWD}/fastq/sequence_file.txt
 unset IFS
 
@@ -83,3 +83,64 @@ done
 # delete empty files
 find ${group}/ -type f -empty -delete
 
+# Create a file that records the results of the grouping
+for item in $(ls ${group})
+do
+echo "$item" >> ${group}/groupinfo1.txt
+paste -s ${group}/$item >> ${group}/groupinfo2.txt
+done
+
+paste ${group}/groupinfo1.txt ${group}/groupinfo2.txt > ${group}/groupinfo3.txt
+awk '{gsub("Tco", "Tco-"); print}' ${group}/groupinfo3.txt > ${group}/groupinfo.txt
+
+rm -f ${group}/groupinfo1.txt ${group}/groupinfo2.txt ${group}/groupinfo3.txt
+
+# 5.2 create gene descriptions file
+echo -e "name\tgene_description" > ${genecounts}/gene_descriptions.txt
+cut -f4,5 ${PWD}/TriTrypDB-46_TcongolenseIL3000_2019.bed >> ${genecounts}/gene_descriptions.txt
+
+# 5.3 calculate the mean expression levels for each group and add gene descriptions
+
+for item in $(ls ${genecounts} | grep "txt")
+do
+cut -f6 ${genecounts}/$item > ${genecounts}/cut.$item
+done
+
+# clear the previous results
+rm -f ${genecounts}/*ed*
+
+
+# the main purpose for this loop: 
+# 1) extract the sixth column of each bedtools output file 
+# 2) paste them together according to grouping outcomes 
+# 3) calculate the mean expression levels
+# 4) add gene descriptions
+
+IFS=$'\t';
+while read groupname item1 item2 item3 item4
+do
+# based on the sample items of each group, there are two conditions : 3 items and 4 items
+# if there are 3 items
+if test -z $item4;
+then
+# create a header of each group counts file
+echo -e "$item1\t$item2\t$item3" > ${genecounts}/${groupname}.temp1
+# according to the group, paste the gene counts files together
+paste ${genecounts}/cut.$item1.gene.counts.txt ${genecounts}/cut.$item2.gene.counts.txt ${genecounts}/cut.$item3.gene.counts.txt >> ${genecounts}/${groupname}.temp1
+# calculate the average of counts for each group
+cat ${genecounts}/${groupname}.temp1 | source ${genecounts}/average.sh > ${genecounts}/${groupname}.temp2
+# Merge the gene description file and the average of the expression levels file
+paste ${genecounts}/gene_descriptions.txt ${genecounts}/${groupname}.temp2 > ${genecounts}/${groupname}.counts
+
+# if there are 4 items
+else
+echo -e "$item1\t$item2\t$item3\t$item4" > ${genecounts}/${groupname}.temp1
+paste ${genecounts}/cut.$item1.gene.counts.txt ${genecounts}/cut.$item2.gene.counts.txt ${genecounts}/cut.$item3.gene.counts.txt ${genecounts}/cut.$item4.gene.counts.txt >> ${genecounts}/${groupname}.temp1
+cat ${genecounts}/${groupname}.temp1 | source ${genecounts}/average.sh > ${genecounts}/${groupname}.temp2
+paste ${genecounts}/gene_descriptions.txt ${genecounts}/${groupname}.temp2 > ${genecounts}/${groupname}.counts
+fi
+done < ${group}/groupinfo.txt
+
+unset IFS
+
+rm -f ${genecounts}/cut* ${genecounts}/temp*
